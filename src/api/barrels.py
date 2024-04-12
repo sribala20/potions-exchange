159690ee145de0acf94d42019aacba580d886973
Sri Bala <srirocks2020@sri-mac.local>
2024-04-12 13:32:31 -0700

    barrels adjusted

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -21,14 +21,18 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
+    barrel_ml = 0
+    price = 0
     for barrel in barrels_delivered:
-        # + mL, - gold
-        with db.engine.begin() as connection:
-            
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :barrel_ml"),
-            {"barrel_ml": barrel.ml_per_barrel})
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :price"),
-            {"price": barrel.price})
+        barrel_ml += (barrel.ml_per_barrel * barrel.quantity)
+        price += (barrel.price * barrel.quantity)
+    
+    # + mL, - gold
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :barrel_ml"),
+        {"barrel_ml": barrel_ml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :price"),
+        {"price": price})
             
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
@@ -39,12 +43,13 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
-
+    
     for barrel in wholesale_catalog:
+        price = barrel.price * barrel.quantity
         with db.engine.begin() as connection:
             potions = connection.execute(sqlalchemy.text("SELECT num_green_potions from global_inventory")).scalar()
             gold = connection.execute(sqlalchemy.text("SELECT gold from global_inventory")).scalar()
-            if potions < 10 and gold >= barrel.price and barrel.sku == "SMALL_GREEN_BARREL":
+            if potions < 10 and gold >= price and barrel.sku == "SMALL_GREEN_BARREL":
                 return [ # purchasing one new small green barrel if num_potions < 10 and gold sufficient
                     {
                             "sku": "SMALL_GREEN_BARREL", 
