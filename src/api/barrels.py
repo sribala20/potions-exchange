a5b818b716a2d15d5214c1406249e4c440d571c5
Sri Bala <srirocks2020@sri-mac.local>
2024-04-15 23:07:28 -0700

    rgb potions for barrel delivery

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -21,20 +21,32 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    barrel_ml = 0
+    red_ml = 0
+    blue_ml = 0
+    green_ml = 0
     price = 0
     for barrel in barrels_delivered:
-        barrel_ml += (barrel.ml_per_barrel * barrel.quantity)
+        if barrel.potion_type == [100,0,0,0]:
+            red_ml += (barrel.ml_per_barrel * barrel.quantity)
+        elif barrel.potion_type == [0,100,0,0]:
+            green_ml += (barrel.ml_per_barrel * barrel.quantity)
+        else:
+            blue_ml += (barrel.ml_per_barrel * barrel.quantity)
         price += (barrel.price * barrel.quantity)
     
     # + mL, - gold
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :barrel_ml"),
-        {"barrel_ml": barrel_ml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml + :red_ml"),
+        {"red_ml": red_ml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml + :green_ml"),
+        {"green_ml": green_ml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml + :blue_ml"),
+        {"blue_ml": blue_ml})
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - :price"),
         {"price": price})
             
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+
 
     return "OK"
 
@@ -54,7 +66,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
         for barrel in wholesale_catalog:
             if potions < 10 and gold >= barrel.price:
-                barrel_lst.append({"sku": barrel.sku, "quantity": 1})
+                barrel_lst.append({"sku": barrel.sku, "quantity": barrel.quantity})
                 gold -= barrel.price
         
         print ("potions = ", potions, ", gold = ",gold)
