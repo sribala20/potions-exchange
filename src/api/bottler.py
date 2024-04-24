2ae82d5db494d51cad3541560369b8b1e944af8d
Sri Bala <srirocks2020@sri-mac.local>
2024-04-24 14:38:30 -0700

    bottler functionality to support bottling purple potions

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -18,29 +18,34 @@ class PotionInventory(BaseModel):
 @router.post("/deliver/{order_id}")
 # posts delivery of potions, order_id = value of single delivery
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
-    num_red = 0
-    num_green = 0
-    num_blue = 0
-    for potion in potions_delivered:
-        if potion.potion_type == [100,0,0,0]:
-            num_red += potion.quantity
-        elif potion.potion_type == [0,100,0,0]:
-            num_green += potion.quantity
-        else:
-            num_blue += potion.quantity
+    num_potions = 0
+    red_ml = 0
+    green_ml = 0
+    blue_ml = 0
 
-        print(potion)
     # - mL, + potions 
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - :red_ml"), {"red_ml": num_red * 100})
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions + :num_red"), {"num_red": num_red})
-        
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml - :green_ml"), {"green_ml": num_green * 100})
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + :num_green"), {"num_green": num_green})
-        
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml - :blue_ml"), {"blue_ml": num_blue * 100})
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = num_blue_potions + :num_blue"), {"num_blue": num_blue})
-        
+        for potion in potions_delivered:
+            num_potions += potion.quantity
+            if potion.potion_type == [100,0,0,0]:
+                red_ml += (potion.quantity * 100)
+            elif potion.potion_type == [0,100,0,0]:
+                green_ml += (potion.quantity * 100)
+            elif potion.potion_type == [0,0,100,0]:
+                blue_ml += (potion.quantity * 100)
+            elif potion.potion_type == [50,0,50,0]:
+                red_ml += (potion.quantity * 50)
+                blue_ml += (potion.quantity * 50)
+            else:
+                raise Exception("Unidentified potion type.")
+            
+            connection.execute(sqlalchemy.text("UPDATE potions WHERE type = :type SET quantity = quantity + :quant"), {"type": potion.potion_type, "quant": potion.quantity})
+            
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - :red_ml"), {"red_ml": red_ml})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_potions = num_potions + :quant"), {"quant": num_potions})
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_ml = num_green_ml - :green_ml"), {"green_ml": green_ml}) 
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_ml = num_blue_ml - :blue_ml"), {"blue_ml": blue_ml})
+    
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
     return "OK"
@@ -51,13 +56,18 @@ def get_bottle_plan():
     Go from barrel to bottle.
     """
     with db.engine.begin() as connection:
-        red_potions = connection.execute(sqlalchemy.text("SELECT num_red_ml from global_inventory")).scalar() // 100
-        green_potions = connection.execute(sqlalchemy.text("SELECT num_green_ml from global_inventory")).scalar() // 100
-        blue_potions = connection.execute(sqlalchemy.text("SELECT num_blue_ml from global_inventory")).scalar() // 100
+        red_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml from global_inventory")).scalar()
+        green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml from global_inventory")).scalar()
+        blue_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml from global_inventory")).scalar()
 
+        purple_usage = min(red_ml//2, blue_ml//2)# mix half of red and blue into purple
+        purple_potions = purple_usage // 50
+        red_ml -= purple_usage
+        blue_ml-= purple_usage
 
-    if red_potions == 0 and green_potions == 0 and blue_potions == 0:
-        return []
+        red_potions = red_ml//10
+        green_potions = green_ml//10
+        blue_potions = blue_ml//10
     
     return [
             {
@@ -71,9 +81,13 @@ def get_bottle_plan():
             {
                 "potion_type": [0, 0, 100, 0],
                 "quantity": blue_potions,
-            }
+            },
+            {
+                "potion_type": [50, 0, 50, 0],
+                "quantity": purple_potions,
+            } # is this hardcoding? 
         ]
 
-
+    
 if __name__ == "__main__":
     print(get_bottle_plan())
