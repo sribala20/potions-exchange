04e37480eff7650d4e4e2a41f86ef24bd68ada12
Sri Bala <srirocks2020@sri-mac.lan>
2024-05-17 17:31:58 -0700

    capacity logic?

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -19,6 +19,13 @@ def get_inventory():
         num_ml = connection.execute(sqlalchemy.text("SELECT COALESCE(SUM(change), 0) FROM ml_ledger")).scalar()
         gold = connection.execute(sqlalchemy.text("SELECT COALESCE(SUM(change), 0) FROM gold_ledger")).scalar()
 
+        connection.execute(sqlalchemy.text("""UPDATE global_inventory SET num_potions = :potions"""),
+                                    {'potions' : num_potions})
+        connection.execute(sqlalchemy.text("""UPDATE global_inventory SET gold = :gold"""),
+                                    {'gold' : gold})
+        connection.execute(sqlalchemy.text("""UPDATE global_inventory SET num_ml = :num_ml"""),
+                                    {'num_ml' : num_ml})
+
     print(f"potions: {num_potions} ml: {num_ml} gold: {gold}")
     return {"number_of_potions": num_potions, "ml_in_barrels": num_ml, "gold": gold}
 
@@ -31,13 +38,29 @@ def get_capacity_plan():
     """
     with db.engine.begin() as connection:
         gold = connection.execute(sqlalchemy.text("SELECT COALESCE(SUM(change), 0) FROM gold_ledger")).scalar()
-    g = 0.5 * gold
-    cap = g // 1000
+        num_potions = connection.execute(sqlalchemy.text("SELECT COALESCE(SUM(change), 0) FROM potion_ledger")).scalar()
+        num_ml = connection.execute(sqlalchemy.text("SELECT COALESCE(SUM(change), 0) FROM ml_ledger")).scalar()
+        curr_ml_cap = connection.execute(sqlalchemy.text("SELECT curr_ml_cap FROM global_inventory")).scalar()
+        curr_pot_cap = connection.execute(sqlalchemy.text("SELECT curr_pot_cap FROM global_inventory")).scalar()
 
+        pot_cap = 0
+        ml_cap = 0
+        g = 0.5 * gold # spend only 1/2 on capacity
+        
+        # check if potions or ml is over 90% current capacity, buys if there is enough gold.
+        if g > 1000:
+            if num_potions >= 0.9(curr_pot_cap):
+                pot_cap += 1
+                g -= 1000
+
+        if g > 1000:
+            if num_ml >= 0.9(curr_ml_cap):
+                ml_cap += 1
+            
 
     return {
-        "potion_capacity": cap,
-        "ml_capacity": cap
+        "potion_capacity": pot_cap,
+        "ml_capacity": ml_cap
         }
 
 class CapacityPurchase(BaseModel):
@@ -52,9 +75,12 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
         with db.engine.begin() as connection:
             connection.execute(sqlalchemy.text('''INSERT INTO gold_ledger (change, description)
                                            VALUES (:change, 'capacity purchased')'''), {"change": payment})
+            connection.execute(sqlalchemy.text("""UPDATE global_inventory SET curr_pot_cap = curr_pot_cap + 1000 """))
+            connection.execute(sqlalchemy.text("""UPDATE global_inventory SET curr_ml_cap = curr_ml_cap + 10000"""))
+       
     """ 
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
-
+    
     return "OK"
